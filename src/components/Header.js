228d31425eaeb075d28d35b128b4f5904d6a98fe
Nathalia <nathalia.cantarelli@edu.pucrs..br>
2022-10-03 23:40:17 -0300

    Requisito 4

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from '../pages/Loading';
 
@@ -27,6 +28,11 @@ class Header extends Component {
         <p data-testid="header-user-name">
           { nameUser !== '' ? nameUser : <Loading /> }
         </p>
+        <Link data-testid="link-to-search" to="/search">Search</Link>
+        <br />
+        <Link data-testid="link-to-favorites" to="/favorites">Favorites</Link>
+        <br />
+        <Link data-testid="link-to-profile" to="/profile">Profile</Link>
       </header>
     );
   }
