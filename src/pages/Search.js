dd7a379049884e82b7f3e587ce722a2253bc92c6
Nathalia <nathalia.cantarelli@edu.pucrs..br>
2022-10-03 23:52:37 -0300

    Requisito 5

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,12 +2,44 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 class Search extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      pesquisa: '',
+      botaoPesquisa: true,
+    };
+  }
+
+  habilitaBotaoPesquisa = ({ target }) => {
+    this.setState({ pesquisa: target.value }, () => {
+      const { pesquisa } = this.state;
+      if (pesquisa.length >= 2) {
+        this.setState({ botaoPesquisa: false });
+      }
+    });
+  };
+
   render() {
+    const { pesquisa, botaoPesquisa } = this.state;
     return (
       <>
         <Header />
         <div data-testid="page-search">
           <p>Search</p>
+          <input
+            type="text"
+            data-testid="search-artist-input"
+            value={ pesquisa }
+            onChange={ this.habilitaBotaoPesquisa }
+          />
+          <button
+            type="button"
+            data-testid="search-artist-button"
+            disabled={ botaoPesquisa }
+          >
+            Pesquisar
+          </button>
         </div>
       </>
     );
